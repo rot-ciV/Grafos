bf295a5da85204debff2dcb88f2f85cab3fc8c38
rot-ciV <fagundes.nvictor@gmail.com>
2026-04-13 13:41:57 -0300

    max tempo e lógica de impressao

--- pert/main.py
+++ pert/main.py
@@ -1,17 +1,124 @@
 import networkx as nx
+import sys
 
 grafo = nx.DiGraph()
 
-with open('grafo.txt', 'r') as arquivo:
+def logica_pert(grafo_utilizado):
+
+    tempo_cedo = {}
+    grau_de_entrada = {}
+    fila = []
+
+    for vertice_atual in grafo_utilizado.nodes:
+
+        tempo_cedo[vertice_atual] = 0
+        grau_de_entrada[vertice_atual] = grafo_utilizado.in_degree[vertice_atual]
+
+        if grau_de_entrada[vertice_atual] == 0:
+            fila.append(vertice_atual)
+
+    while len(fila) > 0:
+
+        vertice_processado = fila.pop(0)
+
+        for vizinho in grafo_utilizado.neighbors(vertice_processado):
+
+            tempo_gasto = grafo_utilizado[vertice_processado][vizinho]['tempo'] + tempo_cedo[vertice_processado]
+
+            if tempo_gasto > tempo_cedo[vizinho]:
+
+                tempo_cedo[vizinho] = tempo_gasto
+
+            grau_de_entrada[vizinho] = grau_de_entrada[vizinho] - 1
+
+            if grau_de_entrada[vizinho] == 0:
+
+                fila.append(vizinho)
+
+    return tempo_cedo
+
+
+# ======= Lógica de Leitura do Arquivo grafos.txt ======= #
+
+grafos_disp = []
+
+with open('grafos.txt', 'r') as arquivo:
 
     for linha in arquivo:
 
-        origem, destino, duracao = linha.split()
+        linha_simples = linha.strip()
 
-        grafo.add_edge(origem, destino, tempo = int(duracao))
+        if linha_simples.startswith('[') and linha_simples.endswith(']'):
+
+            grafos_disp.append(linha_simples)
+
+if len(grafos_disp) == 0:
+    sys.exit('Nenhum grafo foi encontrado.')
 
 
-for vert_atual in grafo.nodes:
+# ======= Lógica de Escolha do Grafo ======= #
 
-    # if vertice atual tiver mais que 1 vizinho, tenho que dividir a  analise
-    # caso contrario só continuo somando 
+
+print('Lista de Grafos disponiveis no arquivo grafos.txt:')
+
+for indice, nome in enumerate(grafos_disp):
+
+    print(f'{indice + 1}. {nome}')
+
+while True:
+
+    try:
+
+        grafo_escolhido = int(input('Digite o número do grafo desejado: ')) - 1
+
+        if grafo_escolhido < 0 or grafo_escolhido >= len(grafos_disp):
+            print('Opção Inválida')
+
+        else:
+            break
+
+
+    except ValueError:
+        print('Opção Inválida')
+
+
+# ======= Lógica de Leitura do Grafo Escolhido ======= #
+
+
+lendo_alvo = False
+
+with open('grafos.txt', 'r') as arquivo:
+
+    for linha in arquivo:
+
+        linha_simples = linha.strip()
+
+        if not linha_simples:
+            continue
+
+        if linha_simples.startswith('[') and linha_simples.endswith(']'):
+
+            if linha_simples == grafos_disp[grafo_escolhido]:
+                lendo_alvo = True
+                continue
+
+            else:
+                lendo_alvo = False
+                continue
+
+        if lendo_alvo:
+            origem, destino, duracao = linha.split() 
+            grafo.add_edge(origem, destino, tempo = int(duracao))
+
+
+# ======= Lógica de Impressão de Resultados ======= #
+
+
+tempo_cedo = logica_pert(grafo)
+
+max_tempo = max(tempo_cedo.values())
+
+print('\n=== Resultados ===')
+print(f'\n Tempo máximo: {max_tempo}')
+
+
