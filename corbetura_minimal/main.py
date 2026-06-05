17cdb6a73b4cd95d3458b75b844acce8c9936c88
rot-ciV <fagundes.nvictor@gmail.com>
2026-06-05 17:34:00 -0300

    algoritmo genetico

--- corbetura_minimal/main.py
+++ corbetura_minimal/main.py
@@ -1,25 +1,187 @@
 import networkx as nx
-import sys
+import random
+
+class Individuo:
+    def __init__(self, dna):
+        self.fitness = 0
+        self.dna = dna
+        self.esquinas_com_camera_ind = set()
+
+def limpaGrafo(Grafo):
+    
+    cameras_colocadas = set()
+
+    while(True):
+
+        esquinas_sem_saida = set()
+        novas_cameras = set()
+
+        for esquina_atual in Grafo.nodes:
+
+            if Grafo.degree[esquina_atual] == 1:
+                esquinas_sem_saida.add(esquina_atual)
+
+        if len(esquinas_sem_saida) == 0:
+            return cameras_colocadas
+
+        for esquina_atual in esquinas_sem_saida:
+            
+            vizinho = list(Grafo.neighbors(esquina_atual))[0]
+            cameras_colocadas.add(vizinho)
+            novas_cameras.add(vizinho)
+
+        for excluir_esquina in novas_cameras:
+
+            ruas_protegidas = list(Grafo.edges(excluir_esquina))
+            Grafo.remove_edges_from(ruas_protegidas)
+
+def criaDNA(Grafo):
+
+    dna = []
+
+    for esquina in Grafo.nodes:
+
+        if Grafo.degree[esquina] > 0:
+            dna.append(esquina)
+
+    random.shuffle(dna)
+    return dna
+
+def calculaFitness(individuo, Grafo):
+
+    grafo_aux = Grafo.copy()
+    flag = 1
+    gene_atual = 0
+    fitness = 0
+
+    while(grafo_aux.number_of_edges() > 0):
+        
+        novas_cameras = limpaGrafo(grafo_aux)
+
+        if(len(novas_cameras) > 0):
+
+            fitness = fitness + len(novas_cameras)
+            individuo.esquinas_com_camera_ind.update(novas_cameras)
+            continue
+
+        else:
+
+            esquina_escolhida = individuo.dna[gene_atual]
+
+            if(grafo_aux.degree[esquina_escolhida] == 0):
+                gene_atual += 1
+            
+            else:
+
+                ruas_perigosas = list(grafo_aux.edges(esquina_escolhida))
+                grafo_aux.remove_edges_from(ruas_perigosas)
+                individuo.esquinas_com_camera_ind.add(esquina_escolhida)
+                gene_atual += 1
+                fitness += 1
+
+    individuo.fitness = fitness
+
+def selecaoPais(populacao):
+    
+    pesos = []
+
+    for individuo in populacao:
+        pesos.append(1 / individuo.fitness)
+
+    return random.choices(populacao, weights=pesos, k = 2)
+
+def criaFilho(pai1, pai2):
+
+    tam_corte = int(len(pai1.dna) * 0.4)
+    limite = len(pai1.dna) - tam_corte
+    inicio = random.randint(0, limite)
+    corte = pai1.dna[inicio : inicio + tam_corte]
+
+    dna_usado = []
+
+    for gene in pai2.dna:
+
+        if(gene not in corte):
+            dna_usado.append(gene)
+
+    ponto_do_corte = random.randint(0, len(dna_usado))
+
+    dna_filho = dna_usado[: ponto_do_corte] + corte + dna_usado[ponto_do_corte:]
+
+    filho = Individuo(dna_filho)
+
+    return filho
+
+def mutacao(individuo, taxa_mutacao=0.05):
+
+    if random.random() < taxa_mutacao:
+
+        gene01 = random.randint(0, len(individuo.dna) - 1)
+        gene02 = random.randint(0, len(individuo.dna) - 1)
+
+        individuo.dna[gene01], individuo.dna[gene02] = individuo.dna[gene02], individuo.dna[gene01]
+
+
+# =============================================================================================== #
 
 caminho_arquivo = "sjdr.gml"
 Grafo = nx.read_gml(caminho_arquivo)
+grafo_limpo = Grafo.copy()
+cameras_base = limpaGrafo(grafo_limpo)
+esquinas_com_camera = set()
 
-total_esquinas = Grafo.number_of_nodes()
-esquinas_sem_saida = set()
-cameras = 0
-
-for esquina_atual in Grafo.nodes:
-
-    if Grafo.degree[esquina_atual] == 1:
-        esquinas_sem_saida.add(esquina_atual)
-
+numero_de_testes = 100
+with open("tabela_de_resultados.txt", "w") as arquivo:
     
-for esquina_atual in esquinas_sem_saida:
+    arquivo.write("===== RESULTADOS DA BATERIA =====\n")
+
+    for teste in range(numero_de_testes):
+
+
+        populacao = []
+
+        for i in range(20):
+
+            individuo = Individuo(criaDNA(grafo_limpo))
+            populacao.append(individuo)
+
+        for individuo in populacao:
+
+            calculaFitness(individuo, grafo_limpo)
+
+        flag = 0
+
+        for geracao in range(100):
+
+            nova_populacao = []
+
+            populacao.sort(key=lambda ind: ind.fitness)
+
+            nova_populacao.append(populacao[0])
+            nova_populacao.append(populacao[1])
+
+            for i in range(9):
+
+                pais = selecaoPais(populacao)
+                filho01 = criaFilho(pais[0], pais[1])
+                filho02 = criaFilho(pais[1], pais[0])
+                mutacao(filho01)
+                mutacao(filho02)
+                nova_populacao.append(filho01)
+                nova_populacao.append(filho02)
+
+            for individuo in nova_populacao:
+
+                if(individuo.fitness == 0):
+                    calculaFitness(individuo, grafo_limpo)
+
+            populacao = nova_populacao
+
+
+        populacao.sort(key=lambda ind: ind.fitness)
+        mais_apto = populacao[0]
+        total_cameras = mais_apto.fitness + len(cameras_base)
+        linha_resultado = f"Teste {teste + 1:02d}: {total_cameras} cameras\n"
+        arquivo.write(linha_resultado)
+        print(f"Teste {teste + 1:02d} concluído com {total_cameras} câmeras.")
     
-    esquina_com_camera = esquina_atual.neigneighbors()[0]
-
-
-
-
-
-
